a01bba577028499f19a2beb42fccd6658d93e83d
martin1123 <martinmaccio89@gmail.com>
2025-05-22 22:12:21 -0300

    Se elimina propiedad extra en Comentario.

--- models/Comentario.js
+++ models/Comentario.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
 const comentarioSchema = mongoose.Schema({
-    idUsuario: { type: String, required: true}, //
-   idNota: { type: String, required: true}, //
+    idUsuario: { type: String, required: true},
+    idNota: { type: String, required: true},
     fecha: { type: Date, required: true},
     texto: { type: String, required: true},
     likes: { type: Number, required: false},
     dislikes: { type: Number, requried: false},
-    denuncias: { type: String, required: false},
-   replies: { type: Comentario, required: false}, //
+    denuncias: { type: String, required: false}
 
 }, { timestamps: true})
 
