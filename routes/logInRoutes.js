9488ad6be52b14a2565cf70b4d1da18598810fd1
martin1123 <martinmaccio89@gmail.com>
2025-06-16 22:13:56 -0300

    Se corrigen errores de dependencias.

--- routes/logInRoutes.js
+++ routes/logInRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express';
-import { protegerRuta } from '../middlewares/uploadMiddleware.js';
-import { allowUpload } from '../middlewares/multer.js';
+import { protegerRuta } from '../middlewares/authMiddlewares.js';
 import { login, refreshToken, logout 
 } from '../controllers/logInController.js'
 
@@ -8,8 +7,8 @@ const router = express.Router()
 
 
 router.post('/api/login', login)
-router.post('/api/refresh', refreshToken); 
-router.post('/api/logout', logout)
+router.post('/api/refresh', protegerRuta, refreshToken); 
+router.post('/api/logout', protegerRuta, logout)
 
 
 export default router
--- routes/notasRoutes.js
+++ routes/notasRoutes.js
@@ -2,16 +2,16 @@ import express from 'express';
 import {
     getNotas,
     getNotasById,
-    CrearNota,
+    crearNota,
     filtrarNotas,
 } from '../controllers/notasController.js';
 import { protegerRuta } from '../middlewares/authMiddlewares.js';
-import { allowMultupleUpload } from '../middlewares/uploadMiddleware.js';
+import { allowMultipleUpload } from '../middlewares/uploadMiddleware.js';
 
 const router = express.Router()
 
 // Crear notas (con imágenes, protegido)
-router.post('/api/notas', protegerRuta, allowMultupleUpload, CrearNota);
+router.post('/api/notas', protegerRuta, allowMultipleUpload, crearNota);
 
 // Obtener todos las notas
 router.get('/api/notas', getNotas);
